3977bcb76be0ba5c1208f227999b720feb1f8336
Alexis Cosentino <alexis27_08@hotmail.fr>
2021-03-26 13:08:15 +0100

    Milestone 3 finished, caroussel added with jQuery plug-in - fixes #4

--- script.js
+++ script.js
@@ -1,58 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    /*
-    $.ajax({
+    a = Array.from({ length: 19 }, () => Math.floor(Math.random() * 600))
 
-            url: "https://rickandmortyapi.com/api/character",
-            method: "GET",
-            dataType : "json",
 
-    })
-
-    .done(function(response){
-        let data = JSON.stringify(response);
-        console.log(data)
-        $("div#rick").append(data[0].name);
-    })
-    .fail(function(error){
-        alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
-    })
-    .always(function(){
-        alert("Requête effectuée");
-    });
-
-    */
-
-    a = Array.from({length: 19}, () => Math.floor(Math.random() * 600))
-
-    console.log(a)
-
-    $.get("https://rickandmortyapi.com/api/character/["+a+"]", function(object){
-        console.log(object);
-        object.forEach(function(item){
+    $.get("https://rickandmortyapi.com/api/character/[" + a + "]", function (object) {
+        object.forEach(function (item) {
             $("<article class='rick'>\
-                <img src='"+item.image+"' alt=''>\
+                <img src='"+ item.image + "' alt=''>\
                 <div class='col'>\
-                <h2> Nom = "+ item.name +" </h2>\
-                <p> Espèce = " + item.species +"</p>\
-                <p> Genre = " + item.gender +"</p>\
-                <p> Planète d'origine = " + item.origin.name +"</p>\
+                <h2>"+ item.name + "</h2>\
+                <p> Espèce = " + item.species + "</p>\
+                <p> Genre = " + item.gender + "</p>\
+                <p> Planète d'origine = " + item.origin.name + "</p>\
                 </div>\
             ").appendTo(".append");
         })
 
     });
 
-    $.get("https://rickandmortyapi.com/api/character/[1,2,3,4,5]", function(object){
-        console.log(object);
-        object.forEach(function(item){
+    
+
+    $.get("https://rickandmortyapi.com/api/character/[1,2,3,4,5]", function (object) {
+        object.forEach(function (item) {
             $("<article class='rick'>\
-                <img src='"+item.image+"' alt=''>\
+                <img src='"+ item.image + "' alt=''>\
                 <div class='col'>\
-                <h2> Nom = "+ item.name +" </h2>\
-                <p> Espèce = " + item.species +"</p>\
-                <p> Genre = " + item.gender +"</p>\
-                <p> Planète d'origine = " + item.origin.name +"</p>\
+                <h2>"+ item.name + "</h2>\
+                <p> Espèce = " + item.species + "</p>\
+                <p> Genre = " + item.gender + "</p>\
+                <p> Planète d'origine = " + item.origin.name + "</p>\
                 </div>\
             ").appendTo(".append");
         })
@@ -63,4 +39,3 @@ $(document).ready(function(){
 });
 
 
-//        console.log(response[0].name);
