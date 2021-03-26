4bff51af98f1edcf6802c65d30bb645ed1516ac0
Alexis Cosentino <alexis27_08@hotmail.fr>
2021-03-26 16:39:29 +0100

    Feat: doing dynamic form. On the way to success, I just need to make it fix because now its disapearing after refresh

--- script.js
+++ script.js
@@ -46,17 +46,23 @@ $(document).ready(function () {
         }
     })
 
-});
 
-/*
-$(".dropbtn").click(function () {
+    //FORMULAIRE DYNAMIQUE
 
-        let click = document.getElementById("list-items");
-        if (click.style.display === "none") {
-            click.style.display = "block";
-        } else {
-            click.style.display = "none";
-        }
+    $(".formbtn").click(function(){
+        let datas = [$(".firstform").val(), $(".secondform").val(), $(".thirdform").val()]
+        console.log(datas[0])
+        $("<article class='rick'>\
+        <h3>"+ datas[0] +"</h3>\
+        <p>" + datas[3] + "</p>\
+        <p> Auteur : " + datas[2] + "</p>\
+        </article>\
+        ").insertAfter(("form"));
     })
-    */
+
+
+
+
+
+});
 
